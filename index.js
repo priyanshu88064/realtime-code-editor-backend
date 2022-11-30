c3022ea24bff8d5339283b1038daae55de153a33
Priyanshu88064 <priyanshu88064@gmail.com>
2022-11-30 23:10:44 +0530

    added socket.io

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const app = require('express')();
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server,{
+    cors:{
+        origin:"*"
+    }
+});
+
 const port = 80;
 
 app.get('/', (req, res) => {
-    res.send("fjakjfdkj");
+    res.send("bhag bsdk");
+});
+
+io.on('connection',(socket)=>{
+    console.log("A user connected.");
+
+    socket.on("join",(roomId)=>{
+        console.log("A user is joining with ID: ",roomId);
+        socket.join(roomId);
+    });
+
+    socket.on("sendEditorData",(data,roomId)=>{
+        socket.to(roomId).emit("getEditorData",data);
+    });
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected");
+    })
 });
 
 server.listen(port,()=>{
     console.log("Server started on ",port);
-})
+});
