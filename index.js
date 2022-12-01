59b4464532c1fa83891c397fdf7051db2c0e0cf1
Priyanshu88064 <priyanshu88064@gmail.com>
2022-12-01 15:07:03 +0530

    Improved Socket Connection

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const app = require('express')();
 const server = require('http').createServer(app);
-
+const port = 80;
 const io = require('socket.io')(server,{
     cors:{
         origin:"*"
     }
 });
 
-const port = 80;
+const roomData = new Map();
+
+// roomData.set("a",["jfakf","faf"]);
+// roomData.set("b",[...roomData.get("b"),"new"]);
+// console.log(roomData.has("b"));
+// console.log(roomData);
 
 app.get('/', (req, res) => {
     res.send("bhag bsdk");
@@ -16,17 +21,32 @@ app.get('/', (req, res) => {
 io.on('connection',(socket)=>{
     console.log("A user connected.");
 
-    socket.on("join",(roomId)=>{
-        console.log("A user is joining with ID: ",roomId);
+    socket.on("join",(roomId,userName)=>{
+        console.log("A user joined with ID: ",roomId);
         socket.join(roomId);
+
+        if(roomData.has(roomId)){
+            roomData.set(roomId,[...roomData.get(roomId),userName]);
+        }else{
+            roomData.set(roomId,[userName]);
+        }
+        io.sockets.in(roomId).emit("sync",roomData.get(roomId));
     });
 
     socket.on("sendEditorData",(data,roomId)=>{
         socket.to(roomId).emit("getEditorData",data);
     });
 
+    socket.on("leave",(roomId,userName)=>{
+        console.log("leave request recieved");
+        socket.leave(roomId);
+        roomData.set(roomId,roomData.get(roomId).filter(c=>userName!==c));
+        socket.to(roomId).emit("sync",roomData.get(roomId));  
+    });
+
     socket.on("disconnect",()=>{
         console.log("User Disconnected");
+        console.log(roomData);
     })
 });
 
